41851000c2e6ae74d8a7e784263e6d22f5a52ae4
murtada9780-droid <murtada9780@gmail.com>
2026-04-11 17:48:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"], # يسمح لـ Vercel ولأي موقع يرسل بيانات
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
